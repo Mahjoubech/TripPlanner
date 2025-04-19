042b2b73151c2a347673fa5b8a25ac385364b5e5
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-04-19 09:54:06 +0100

    TRIP-3 <create migration all ao tables i need>

--- database/migrations/2025_04_19_013446_trip_tronsports.php
+++ database/migrations/2025_04_19_013446_trip_tronsports.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('trip_transports', function (Blueprint $table) {
             $table->id();
             $table->foreignId('trip_id')->constrained('trips')->onDelete('cascade');
-            $table->foreignId('transport_id')->constrained('tronsports')->onDelete('cascade');
+            $table->foreignId('transport_id')->constrained('transports')->onDelete('cascade');
             $table->timestamps();
         });
     }
