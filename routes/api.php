cb7aab1b450dbd1369fa3be2f49925bf589d153e
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-04-19 21:52:35 +0100

    create route for auth(login and register and logout)

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,13 @@
 <?php
+use App\Http\Controllers\AuthController;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+Route::post('/register-client', [AuthController::class, 'registerClient']);
+Route::post('/register-organizer', [AuthController::class, 'registerOrganizer']);
+Route::post('/login', [AuthController::class, 'login']);
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:api')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::get('/me', [AuthController::class, 'me']);
+    Route::post('/update-profile', [AuthController::class, 'updateProfile']);
 });
