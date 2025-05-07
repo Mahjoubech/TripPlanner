acd775d4df7638fd59960096e715bb4b6e436514
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-05-07 05:46:49 +0100

    display upcommingTrip

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -1,13 +1,24 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Trip;
 
 class ClientController extends Controller
 {
-    public function dashboard()
-    {
-        return view('client.dashboard');
+   
+        public function dashboard()
+     {
+    $upcomingTrips = Trip::whereHas('bookings', function($q) {
+        $q->where('user_id', auth()->id());
+    })
+    ->where('start_date', '>=', now())
+    ->orderBy('start_date')
+    ->take(5)
+    ->get();
+
+    return view('client.dashboard', compact('upcomingTrips'));
     }
+    
     public function profile(){
         return view('client.profile.profile');
     }
