d2fd5748b632fcd00a08d8c38357fcdbfb97936c
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-05-07 05:45:35 +0100

    remove payment method

--- database/migrations/2025_04_19_013314_bookings.php
+++ database/migrations/2025_04_19_013314_bookings.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->string('status');
             $table->integer('participants');
             $table->decimal('total_price', 10, 2);
-            $table->string('payment_method')->nullable();
             $table->string('payment_id')->nullable();
             $table->text('notes')->nullable();
             $table->timestamps();
